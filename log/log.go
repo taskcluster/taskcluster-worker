7097159a82a875fa00212966b60f078110bbabec
Greg Arndt <arndt.greg@gmail.com>
2016-02-02 16:36:08 +0100

    Add mutex and doc strings

--- log/log.go
+++ log/log.go
@@ -4,24 +4,35 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"sync"
 	"time"
 )
 
 type loggingLevel int
 
+// Log messages can have one of 3 possible states.
 const (
 	DEBUG loggingLevel = 1 << iota
 	INFO
 	CRITICAL
 )
 
+// List of fields that should not be overwritten by fields specified when logging
+// a message
 var restrictedFields = []string{"level", "time", "message"}
 
+// Creates a new logging instance
 func NewLogger(out io.Writer, level loggingLevel, fields map[string]interface{}) *logger {
-	return &logger{out, fields, level}
+	return &logger{
+		out:           out,
+		defaultFields: fields,
+		level:         level,
+	}
 }
 
 type logger struct {
+	// Create a lock when writing to `out` so output is not intermingled
+	mu  sync.Mutex
 	out io.Writer
 	// List of default fields that will be serialized
 	// along with the message and written to `out`
@@ -29,6 +40,7 @@ type logger struct {
 	level         loggingLevel
 }
 
+// Log a debug message as long as the logger's debug level is set to at least Debug
 func (l *logger) Debug(message string, fields map[string]interface{}) {
 	if l.level > DEBUG {
 		return
@@ -38,6 +50,7 @@ func (l *logger) Debug(message string, fields map[string]interface{}) {
 	l.Write(f)
 }
 
+// Log an informational message as long as the logger's debug level is set to at least Info
 func (l *logger) Info(message string, fields map[string]interface{}) {
 	if l.level > INFO {
 		return
@@ -47,6 +60,8 @@ func (l *logger) Info(message string, fields map[string]interface{}) {
 	l.Write(f)
 }
 
+// Log a critical message.  Critical Messages will have "[alert-operator]" prepended to them
+// for alerting purposes.
 func (l *logger) Critical(message string, fields map[string]interface{}) {
 	f := l.createMessage(message, fields)
 	f["level"] = "critical"
@@ -56,11 +71,19 @@ func (l *logger) Critical(message string, fields map[string]interface{}) {
 func (l *logger) Write(message map[string]interface{}) {
 	// ignore if there is an error, logging failure should not cause something fatal
 	if output, err := json.Marshal(message); err == nil {
-		l.out.Write(output)
-		l.out.Write([]byte("\n"))
+		l.mu.Lock()
+		defer l.mu.Unlock()
+		if _, err := l.out.Write(output); err != nil {
+			fmt.Println("Error writing to out")
+		}
+		if _, err := l.out.Write([]byte("\n")); err != nil {
+			fmt.Println("Error writing to out")
+		}
 	}
 }
 
+// Creates a map with the default fields along with any fields that were added at
+// the time of logging the message.
 func (l *logger) createMessage(message string, fields map[string]interface{}) map[string]interface{} {
 	f := map[string]interface{}{
 		"message": message,
