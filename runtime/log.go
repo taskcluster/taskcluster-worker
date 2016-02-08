2fd55745da321c9f766c9aedfb893686dde72f8a
Greg Arndt <arndt.greg@gmail.com>
2016-02-08 09:23:28 -0600

    Made logging level parsing error message clearer

--- runtime/log.go
+++ runtime/log.go
@@ -6,7 +6,7 @@ import (
 	"github.com/Sirupsen/logrus"
 )
 
-// Create a logger that can be passed around through the environment.
+// CreateLogger returns a new logger that can be passed around through the environment.
 // Loggers can be created based on the one returned from this method by calling
 // WithField or WithFields and specifying additional fields that the package
 // would like.
@@ -17,7 +17,7 @@ func CreateLogger(level string) (*logrus.Logger, error) {
 
 	lvl, err := logrus.ParseLevel(level)
 	if err != nil {
-		return nil, fmt.Errorf("Unable to parse logging level: %s\n", level)
+		return nil, fmt.Errorf("Error: Invalid logging level '%s'.\n", level)
 	}
 
 	logger := logrus.New()
