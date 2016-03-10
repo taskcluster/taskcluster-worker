c7f21cb9df4c994ccb5f19d8de1af8d61f21a5e0
Brian Stack <bstack@mozilla.com>
2016-03-10 10:07:41 -0800

    Add a mechanism for testing plugins

--- plugins/env/env_test.go
+++ plugins/env/env_test.go
@@ -1,79 +1,44 @@
 package env
 
 import (
-	"encoding/json"
-	"fmt"
-	"github.com/stretchr/testify/assert"
-	"github.com/taskcluster/taskcluster-worker/engines"
-	"github.com/taskcluster/taskcluster-worker/plugins"
-	"github.com/taskcluster/taskcluster-worker/runtime"
+	"github.com/taskcluster/taskcluster-worker/plugins/plugintest"
 	"testing"
 )
 
-type mockSandboxBuilder struct {
-	engines.SandboxBuilderBase
-	vars map[string]string
+func TestEnvDefinition(*testing.T) {
+	plugintest.Case{
+		Payload: `{
+			"start": {
+				"delay": 0,
+				"function": "print-env-var",
+				"argument": "ENV1"
+			},
+			"env": {
+				"ENV1": "env1"
+			}
+		}`,
+		Plugin:        "env",
+		PluginSuccess: true,
+		EngineSuccess: true,
+		MatchLog:      "env1",
+	}.Test()
 }
 
-func (s mockSandboxBuilder) SetEnvironmentVariable(k, v string) error {
-	s.vars[k] = v
-	return nil
-}
-
-func (s mockSandboxBuilder) GetEnvironmentVariable(k string) string {
-	return s.vars[k]
-}
-
-func (s mockSandboxBuilder) StartSandbox() (engines.Sandbox, error) {
-	return nil, engines.ErrFeatureNotSupported
-}
-
-func TestPluginPayloadSchema(t *testing.T) {
-	assert := assert.New(t)
-
-	p := plugin{}
-
-	schema, err := p.PayloadSchema()
-	assert.Equal(err, nil, err)
-
-	data := map[string]json.RawMessage{
-		"env": json.RawMessage(`{
-			"ENV1": "env1",
-			"ENV2": "env2"
-		}`),
-	}
-
-	parsedData, err := schema.Parse(data)
-	assert.Equal(err, nil, fmt.Sprintf("%v", err))
-	assert.NotEqual(parsedData, nil, "Parsed data must not be nil")
-
-	badData := map[string]json.RawMessage{
-		"env": json.RawMessage(`{
-			"ENV1": 1
-		}`),
-	}
-
-	_, err = schema.Parse(badData)
-	assert.NotEqual(err, nil, "We should have an error here")
-}
-
-func TestTaskPluginPrepare(t *testing.T) {
-	assert := assert.New(t)
-	p := plugin{}
-	tc, err := p.NewTaskPlugin(plugins.TaskPluginOptions{
-		TaskInfo: &runtime.TaskInfo{},
-		Payload:  envPayload{"ENV1": "env1", "ENV2": "env2"},
-	})
-	assert.Equal(err, nil, fmt.Sprintf("%v", err))
-
-	sandboxBuilder := mockSandboxBuilder{
-		engines.SandboxBuilderBase{},
-		map[string]string{},
-	}
-
-	err = tc.BuildSandbox(sandboxBuilder)
-	assert.Equal(err, nil, fmt.Sprintf("%v", err))
-
-	assert.Equal(sandboxBuilder.GetEnvironmentVariable("ENV1"), "env1")
-	assert.Equal(sandboxBuilder.GetEnvironmentVariable("ENV2"), "env2")
+func TestEnvUnDefinition(*testing.T) {
+	plugintest.Case{
+		Payload: `{
+			"start": {
+				"delay": 0,
+				"function": "print-env-var",
+				"argument": "ENV1"
+			},
+			"env": {
+				"ENV2": "env2"
+			}
+		}`,
+		Plugin:        "env",
+		PluginSuccess: true,
+		EngineSuccess: false,
+		NotMatchLog:   "env1",
+	}.Test()
 }
