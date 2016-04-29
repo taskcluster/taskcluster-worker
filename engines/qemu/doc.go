f7a94640cb2a90cc3218b9231415a31f55e91d76
Jonas Finnemann Jensen <jopsen@gmail.com>
2016-04-29 11:03:05 -0700

    Tests now running reliably under vagrant

--- engines/qemu/doc.go
+++ engines/qemu/doc.go
@@ -1,2 +1,9 @@
 // Package qemu implements a QEMU based engine for taskcluster-worker.
+//
+// This package requires following debian packages:
+//  - qemu
+//  - iproute2
+//  - dnsmasq-base
+// This is tested against Debian Jessie 64bit, should probably work with most
+// other systems.
 package qemu
