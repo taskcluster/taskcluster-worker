8e926a8797dc883c7ef7966be16b009b5d65b943
Greg Arndt <arndt.greg@gmail.com>
2016-05-18 14:29:02 -0500

    Tasks should be reclaimed on an interval

--- worker/taskmanager_test.go
+++ worker/taskmanager_test.go
@@ -14,6 +14,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/taskcluster/slugid-go/slugid"
 	"github.com/taskcluster/taskcluster-client-go/queue"
+	"github.com/taskcluster/taskcluster-client-go/tcclient"
 	"github.com/taskcluster/taskcluster-worker/config"
 	"github.com/taskcluster/taskcluster-worker/engines/extpoints"
 	_ "github.com/taskcluster/taskcluster-worker/plugins/artifacts"
@@ -91,9 +92,10 @@ func TestTaskManagerRunTask(t *testing.T) {
 				ClientID:    "abc",
 				Certificate: "",
 			},
+			TakenUntil: tcclient.Time(time.Now().Add(time.Minute * 5)),
 		},
 		definition: &queue.TaskDefinitionResponse{
-			Payload: []byte(`{"start": {"delay": 10,"function": "write-log","argument": "Hello World"}}`),
+			Payload: []byte(`{"start": {"delay": 1,"function": "write-log","argument": "Hello World"}}`),
 		},
 	}
 	tm.run(claim)
@@ -160,6 +162,7 @@ func TestCancelTask(t *testing.T) {
 				ClientID:    "abc",
 				Certificate: "",
 			},
+			TakenUntil: tcclient.Time(time.Now().Add(time.Minute * 5)),
 		},
 		definition: &queue.TaskDefinitionResponse{
 			Payload: []byte(`{"start": {"delay": 5000,"function": "write-log","argument": "Hello World"}}`),
@@ -255,6 +258,7 @@ func TestWorkerShutdown(t *testing.T) {
 					ClientID:    "abc",
 					Certificate: "",
 				},
+				TakenUntil: tcclient.Time(time.Now().Add(time.Minute * 5)),
 			},
 		},
 		&taskClaim{
@@ -273,6 +277,7 @@ func TestWorkerShutdown(t *testing.T) {
 					ClientID:    "abc",
 					Certificate: "",
 				},
+				TakenUntil: tcclient.Time(time.Now().Add(time.Minute * 5)),
 			},
 		},
 	}
