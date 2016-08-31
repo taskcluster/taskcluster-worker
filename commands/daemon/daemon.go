cfccf8da8946aecc605e413fa8707ed69f65d22c
Wander Lairson Costa <wcosta@mozilla.com>
2016-08-31 13:37:46 -0300

    Bug 1298801: Includes the daemon command. r=jonasfj
    This commits introduces the ability for taskcluster-worker to run as a
    daemon. Before running it, you need to install it as a daemon, with the
    "install" command:
    
    $ taskcluster-worker daemon install --engine macosx --logging-level info
    
    The --engine argument is mandatory for the install command. To start the
    daemon, run:
    
    $ taskcluster-worker daemon start
    
    To stop it:
    
    $ taskcluster-worker daemon stop
    
    And to uninstall it:
    
    $ taskcluster-worker daemon remove
    
    Remember taskcluster-worker reads taskcluster credentials from
    environment variables. You need to make sure taskcluster-worker daemon
    has these environment variables set.

---
+++ commands/daemon/daemon.go
@@ -0,0 +1,70 @@
+package daemon
+
+import (
+	"fmt"
+	"os"
+
+	daemonize "github.com/takama/daemon"
+	cmd_ep "github.com/taskcluster/taskcluster-worker/commands/extpoints"
+	"github.com/taskcluster/taskcluster-worker/runtime"
+)
+
+const (
+	// name of the service
+	name        = "taskcluster-worker"
+	description = "Taskcluster worker"
+)
+
+var dependencies = []string{}
+
+func init() {
+	cmd_ep.CommandProviders.Register(cmd{}, "daemon")
+}
+
+type cmd struct{}
+
+func (cmd) Summary() string {
+	return "Run as a daemon."
+}
+
+func usage() string {
+	return `Usage:
+  taskcluster-worker daemon (install | run) <engine> [--logging-level <level>]
+  taskcluster-worker daemon (start | stop | remove)
+
+Options:
+  -l <level>, --logging-level=<level>   Logging level [default: info]
+`
+}
+
+func (cmd) Usage() string {
+	return usage()
+}
+
+func (cmd) Execute(args map[string]interface{}) bool {
+	// set up logger
+	var level string
+	if l := args["--logging-level"]; l != nil {
+		level = l.(string)
+	}
+	logger, err := runtime.CreateLogger(level)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	srv, err := daemonize.New(name, description, dependencies...)
+	if err != nil {
+		logger.Fatal("Error: ", err)
+	}
+
+	svc := &service{srv, args, logger}
+	status, err := svc.Manage()
+
+	if err != nil {
+		logger.Fatalf("%s\n%v", status, err)
+	}
+
+	logger.Info(status)
+	return true
+}
