ff49ba415c4ebd5133b83f5b39123b41f59971da
Jonas Finnemann Jensen <jopsen@gmail.com>
2016-09-23 14:19:01 -0700

    updated vendor/ adding go-schematypes: AnyOf, AllOf, OneOf

--- vendor/github.com/taskcluster/go-schematypes/schema.go
+++ vendor/github.com/taskcluster/go-schematypes/schema.go
@@ -6,12 +6,3 @@ type Schema interface {
 	Validate(data interface{}) error
 	Map(data, target interface{}) error
 }
-
-// An AnyOf instance represents the anyOf JSON schema construction.
-type AnyOf []Schema
-
-// A OneOf instance represents the oneOf JSON schema construction.
-type OneOf []Schema
-
-// An AllOf instance represents the allOf JSON schema construction.
-type AllOf []Schema
