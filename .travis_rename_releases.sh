5e37c67aa0d8b340f47ceb7ef9d1b0004ab436e0
Wander Lairson Costa <wcosta@mozilla.com>
2017-01-23 19:38:47 -0200

    Add the version number to the release file. r=pmoore

--- .travis_rename_releases.sh
+++ .travis_rename_releases.sh
@@ -12,5 +12,5 @@ FILE_EXT=""
 # let's rename the release file because it has a 1:1 mapping with what it is called on
 # github releases, and therefore the name for each platform needs to be unique so that
 # they don't overwrite each other. Set a variable that can be used in .travis.yml
-export RELEASE_FILE="${TRAVIS_BUILD_DIR}/taskcluster-worker-${GOOS}-${GOARCH}${FILE_EXT}"
+export RELEASE_FILE="${TRAVIS_BUILD_DIR}/taskcluster-worker-${TRAVIS_TAG:1}-${GOOS}-${GOARCH}${FILE_EXT}"
 mv "${TRAVIS_BUILD_DIR}/taskcluster-worker${FILE_EXT}" "${RELEASE_FILE}"
