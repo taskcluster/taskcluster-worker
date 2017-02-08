fe830cfcb87b8e3604d2f1a50a88719c2e9903d2
Wander Lairson Costa <wcosta@mozilla.com>
2017-02-08 09:46:27 -0200

    Bug 1336585 part 4: Rename homeFolder to workingFolder.
    As the working folder doesn't necessarily matches the home folder in
    case we don't create a new user to run the task, we change nomenclature
    to make the semantics clear.

--- engines/native/resultset.go
+++ engines/native/resultset.go
@@ -14,12 +14,12 @@ import (
 
 type resultSet struct {
 	engines.ResultSetBase
-	engine     *engine
-	context    *runtime.TaskContext
-	log        *logrus.Entry
-	homeFolder runtime.TemporaryFolder
-	user       *system.User
-	success    bool
+	engine        *engine
+	context       *runtime.TaskContext
+	log           *logrus.Entry
+	workingFolder runtime.TemporaryFolder
+	user          *system.User
+	success       bool
 }
 
 func (r *resultSet) Success() bool {
@@ -28,7 +28,7 @@ func (r *resultSet) Success() bool {
 
 func (r *resultSet) ExtractFile(path string) (ioext.ReadSeekCloser, error) {
 	// Evaluate symlinks
-	p, err := filepath.EvalSymlinks(filepath.Join(r.homeFolder.Path(), path))
+	p, err := filepath.EvalSymlinks(filepath.Join(r.workingFolder.Path(), path))
 	if err != nil {
 		if _, ok := err.(*os.PathError); ok {
 			return nil, engines.ErrResourceNotFound
@@ -41,12 +41,12 @@ func (r *resultSet) ExtractFile(path string) (ioext.ReadSeekCloser, error) {
 	// Cleanup the path
 	p = filepath.Clean(p)
 
-	prefix, err := filepath.EvalSymlinks(r.homeFolder.Path() + string(filepath.Separator))
+	prefix, err := filepath.EvalSymlinks(r.workingFolder.Path() + string(filepath.Separator))
 	if err != nil {
 		panic(err)
 	}
 
-	// Check that p is inside homeFolder
+	// Check that p is inside workingFolder
 	if !strings.HasPrefix(p, prefix) {
 		return nil, engines.ErrResourceNotFound
 	}
@@ -72,7 +72,7 @@ func (r *resultSet) ExtractFile(path string) (ioext.ReadSeekCloser, error) {
 
 func (r *resultSet) ExtractFolder(path string, handler engines.FileHandler) error {
 	// Evaluate symlinks
-	p, err := filepath.EvalSymlinks(filepath.Join(r.homeFolder.Path(), path))
+	p, err := filepath.EvalSymlinks(filepath.Join(r.workingFolder.Path(), path))
 	if err != nil {
 		if _, ok := err.(*os.PathError); ok {
 			return engines.ErrResourceNotFound
@@ -85,12 +85,12 @@ func (r *resultSet) ExtractFolder(path string, handler engines.FileHandler) erro
 	// Cleanup the path
 	p = filepath.Clean(p)
 
-	prefix, err := filepath.EvalSymlinks(r.homeFolder.Path() + string(filepath.Separator))
+	prefix, err := filepath.EvalSymlinks(r.workingFolder.Path() + string(filepath.Separator))
 	if err != nil {
 		panic(err)
 	}
 
-	// Check that p is inside homeFolder
+	// Check that p is inside workingFolder
 	if !strings.HasPrefix(p, prefix) {
 		return engines.ErrResourceNotFound
 	}
@@ -154,7 +154,7 @@ func (r *resultSet) Dispose() error {
 	}
 
 	// Remove temporary home folder
-	if rerr := r.homeFolder.Remove(); rerr != nil {
+	if rerr := r.workingFolder.Remove(); rerr != nil {
 		r.log.Error("Failed to remove temporary home directory, error: ", rerr)
 		err = rerr
 	}
