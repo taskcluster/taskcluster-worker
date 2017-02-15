2f15a1a95f60c49e1ea40039736098389df0b5cc
Pete Moore <pmoore@mozilla.com>
2017-02-15 13:40:05 +0100

    Increase signal channel buffer size to match number of signals trapped

--- commands/work/work.go
+++ commands/work/work.go
@@ -44,7 +44,7 @@ func (cmd) Execute(args map[string]interface{}) bool {
 		w.Start()
 		close(done)
 	}()
-	c := make(chan os.Signal, 1)
+	c := make(chan os.Signal, 2)
 	signal.Notify(c, os.Interrupt, os.Kill)
 	select {
 	case <-c:
