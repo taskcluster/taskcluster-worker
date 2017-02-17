b2896bbf5de99e0604e22fa37c408f3c33dcb911
Pete Moore <pmoore@mozilla.com>
2017-02-17 22:08:36 +0100

    wip

---
+++ integrationtest/artifacts_test.go
@@ -0,0 +1,27 @@
+package integrationtest
+
+import (
+	"testing"
+
+	"github.com/taskcluster/slugid-go/slugid"
+)
+
+var (
+	// all tests can share taskGroupId so we can view all test tasks in same
+	// graph later for troubleshooting
+	taskGroupID string = slugid.Nice()
+)
+
+func TestUpload(t *testing.T) {
+
+	payload := TaskPayload{
+		Command: []string{
+			"echo",
+			"hello world",
+		},
+	}
+	task, workerType := NewTestTask("TestUpload")
+	taskID, _ := SubmitTask(t, task, payload)
+	RunTestWorker(workerType)
+	t.Log(taskID)
+}
