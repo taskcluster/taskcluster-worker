80b00a71ccd4d7f0451ce6eae5105265481d7b3c
Jonas Finnemann Jensen <jopsen@gmail.com>
2017-02-24 16:01:15 -0800

    Added MockAuth implementation

--- runtime/client/auth.go
+++ runtime/client/auth.go
@@ -1,10 +1,30 @@
 package client
 
-import "github.com/taskcluster/taskcluster-client-go/auth"
+import (
+	"github.com/stretchr/testify/mock"
+	"github.com/taskcluster/taskcluster-client-go/auth"
+)
 
 // Auth interface covers parts of the auth.Auth client that we use. This allows
 // us to mock the implementation during tests.
 type Auth interface {
 	SentryDSN(project string) (*auth.SentryDSNResponse, error)
 	StatsumToken(project string) (*auth.StatsumTokenResponse, error)
+}
+
+// MockAuth is a mock implementation of Auth for testing.
+type MockAuth struct {
+	mock.Mock
+}
+
+// SentryDSN is a mock implementation of SentryDSN that calls into m.Mock
+func (m *MockAuth) SentryDSN(project string) (*auth.SentryDSNResponse, error) {
+	args := m.Called(project)
+	return args.Get(0).(*auth.SentryDSNResponse), args.Error(1)
+}
+
+// StatsumToken is a mock implementation of StatsumToken that calls into Mock
+func (m *MockAuth) StatsumToken(project string) (*auth.StatsumTokenResponse, error) {
+	args := m.Called(project)
+	return args.Get(0).(*auth.StatsumTokenResponse), args.Error(1)
 }
