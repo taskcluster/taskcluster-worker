ad01105c0ede66569a694d34e1fb9618a2cd6c8a
Jonas Finnemann Jensen <jopsen@gmail.com>
2017-02-28 13:51:07 -0800

    Use monitor in tests for qemu image manager

--- engines/qemu/image/manager_test.go
+++ engines/qemu/image/manager_test.go
@@ -11,10 +11,10 @@ import (
 	"time"
 
 	"github.com/Sirupsen/logrus"
-	"github.com/getsentry/raven-go"
 	"github.com/stretchr/testify/require"
 	"github.com/taskcluster/slugid-go/slugid"
 	"github.com/taskcluster/taskcluster-worker/runtime/gc"
+	"github.com/taskcluster/taskcluster-worker/runtime/mocks"
 )
 
 const testImageFile = "../test-image/tinycore-worker.tar.zst"
@@ -23,11 +23,11 @@ func TestImageManager(t *testing.T) {
 	debug(" - Setup environment needed to test")
 	gc := &gc.GarbageCollector{}
 	log := logrus.StandardLogger()
-	sentry, _ := raven.New("")
+	monitor := mocks.NewMockMonitor(true)
 	imageFolder := filepath.Join("/tmp", slugid.Nice())
 
 	debug(" - Create manager")
-	manager, err := NewManager(imageFolder, gc, log.WithField("subsystem", "image-manager"), sentry)
+	manager, err := NewManager(imageFolder, gc, log.WithField("subsystem", "image-manager"), monitor)
 	require.NoError(t, err, "Failed to create image manager")
 
 	debug(" - Test parallel download")
