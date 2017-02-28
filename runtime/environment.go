8fbfdfc962a855fe53472ed2e4729d911422e228
Jonas Finnemann Jensen <jopsen@gmail.com>
2017-02-28 14:08:58 -0800

    Remove logrus from runtime.Environment, we should be using monitor

--- runtime/environment.go
+++ runtime/environment.go
@@ -1,7 +1,6 @@
 package runtime
 
 import (
-	"github.com/Sirupsen/logrus"
 	"github.com/taskcluster/taskcluster-worker/runtime/gc"
 	"github.com/taskcluster/taskcluster-worker/runtime/webhookserver"
 )
@@ -9,11 +8,7 @@ import (
 // Environment is a collection of objects that makes up a runtime environment.
 type Environment struct {
 	GarbageCollector gc.ResourceTracker
-	//TODO: Add some sort of interface to the system logger
-	//TODO: Add some interface to submit statistics for influxdb/signalfx
-	//TODO: Add some interface to attach a http.Handler to public facing server
 	TemporaryStorage
-	Log *logrus.Logger
 	webhookserver.WebHookServer
 	Monitor
 }
