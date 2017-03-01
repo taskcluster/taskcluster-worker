5ac279aa1bd469e42cefc56e347677bae6f0a8a8
Pete Moore <pmoore@mozilla.com>
2017-03-01 12:05:33 +0100

    Add first integration test (#172)

---
+++ test/artifacts_test.go
@@ -0,0 +1,41 @@
+package integrationtest
+
+import (
+	"path/filepath"
+	"runtime"
+	"testing"
+
+	tcclient "github.com/taskcluster/taskcluster-client-go"
+	_ "github.com/taskcluster/taskcluster-worker/plugins/artifacts"
+	_ "github.com/taskcluster/taskcluster-worker/plugins/livelog"
+)
+
+func TestUpload(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		t.Skip("Currently not supported on Windows")
+	}
+
+	task, workerType := NewTestTask("TestUpload")
+	payload := TaskPayload{
+		Command: []string{
+			"pwd",
+		},
+		Artifacts: []struct {
+			Expires tcclient.Time `json:"expires,omitempty"`
+			Name    string        `json:"name"`
+			Path    string        `json:"path"`
+			Type    string        `json:"type"`
+		}{
+			{
+				Expires: task.Expires,
+				Name:    "SampleArtifacts/_/X.txt",
+				Path:    filepath.Join(testdata, "SampleArtifacts/_/X.txt"),
+				Type:    "file",
+			},
+		},
+	}
+	SubmitTask(t, task, payload)
+	RunTestWorker(workerType)
+
+	// now check
+}
