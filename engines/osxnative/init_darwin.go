52ef40faa712e5cbd77ff393727fb7bf146dad1c
Pete Moore <pmoore@mozilla.com>
2017-03-01 12:21:02 +0100

    Removed osxnative engine

--- engines/osxnative/init_darwin.go
+++
@@ -1,8 +0,0 @@
-package osxnative
-
-import "github.com/taskcluster/taskcluster-worker/engines"
-
-func init() {
-	// Register the mac engine as an import side-effect
-	engines.Register("macosx", engineProvider{})
-}
