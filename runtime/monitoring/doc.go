d17786adf8b55cf42beff3dd3674e3e5ffca1d95
Jonas Finnemann Jensen <jopsen@gmail.com>
2017-04-10 13:07:18 -0700

    Removed some unused code and minor doc comments

--- runtime/monitoring/doc.go
+++ runtime/monitoring/doc.go
@@ -1,7 +1,7 @@
 // Package monitoring provides multiple implementations of runtime.Monitor.
 //
 // In addition to supplying runtime.Monitor implementations this package also
-// provides and a ConfigSchema and a generic New(config) method that can be
+// provides a ConfigSchema and a generic New(config) method that can be
 // used to instantiate one of the implementations dependening on configuration.
 // This allows for configurable selection of monitoring strategy without
 // complicating the application with configuration.
