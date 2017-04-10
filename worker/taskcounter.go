f9832b88b75efd4a98c68ae26c457ddcef7c0296
Jonas Finnemann Jensen <jopsen@gmail.com>
2017-04-10 13:13:47 -0700

    Removed atomics.StopWatch

--- worker/taskcounter.go
+++ worker/taskcounter.go
@@ -3,8 +3,6 @@ package worker
 import (
 	"sync"
 	"time"
-
-	"github.com/taskcluster/taskcluster-worker/runtime/atomics"
 )
 
 // taskCounter keeps count of number of active tasks as well as idle time.
@@ -12,31 +10,35 @@ type taskCounter struct {
 	m         sync.Mutex
 	c         sync.Cond
 	value     int
-	idleTimer atomics.StopWatch
+	idleSince time.Time
 }
 
-// initialize the taskCounter, if not already initialized
-func (c *taskCounter) init() {
+// initialize the taskCounter and lock it, if not already initialized
+func (c *taskCounter) initAndLock() {
 	c.m.Lock()
-	defer c.m.Unlock()
 
 	if c.c.L == nil {
 		c.c.L = &c.m
-		c.idleTimer.Start()
+		c.idleSince = time.Now()
 	}
 }
 
 // IdleTime returns the duration since last time the worker was working on a
 // task. Zero if the worker is currently working.
 func (c *taskCounter) IdleTime() time.Duration {
-	c.init()
-	return c.idleTimer.Elapsed()
+	c.initAndLock()
+	defer c.m.Unlock()
+
+	// if task counter is higher than zero, then we're not idle
+	if c.value > 0 {
+		return 0
+	}
+	return time.Since(c.idleSince)
 }
 
 // WaitForIdle blocks until the active task count is zero
 func (c *taskCounter) WaitForIdle() {
-	c.init()
-	c.m.Lock()
+	c.initAndLock()
 	defer c.m.Unlock()
 
 	for c.value > 0 {
@@ -46,8 +48,7 @@ func (c *taskCounter) WaitForIdle() {
 
 // WaitForLessThan blocks until the active task count is less than given value
 func (c *taskCounter) WaitForLessThan(value int) {
-	c.init()
-	c.m.Lock()
+	c.initAndLock()
 	defer c.m.Unlock()
 
 	for c.value >= value {
@@ -57,25 +58,20 @@ func (c *taskCounter) WaitForLessThan(value int) {
 
 // Increment the active task count
 func (c *taskCounter) Increment() {
-	c.init()
-	c.m.Lock()
+	c.initAndLock()
 	defer c.m.Unlock()
 
 	c.value++
-	c.idleTimer.Reset()
 	c.c.Broadcast()
 }
 
 // Decrement the active task count
 func (c *taskCounter) Decrement() {
-	c.init()
-	c.m.Lock()
+	c.initAndLock()
 	defer c.m.Unlock()
 
 	c.value--
-	if c.value == 0 {
-		c.idleTimer.Start()
-	}
+	c.idleSince = time.Now()
 	c.c.Broadcast()
 	if c.value < 0 {
 		panic("worker.taskCounter should never be able to go below zero")
@@ -84,8 +80,7 @@ func (c *taskCounter) Decrement() {
 
 // Value returns the number active tasks
 func (c *taskCounter) Value() int {
-	c.init()
-	c.m.Lock()
+	c.initAndLock()
 	defer c.m.Unlock()
 
 	return c.value
