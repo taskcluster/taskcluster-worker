d3ca825d01e21edb4fff1fbbdd8e1bade7018558
Jonas Finnemann Jensen <jopsen@gmail.com>
2017-05-03 17:16:52 -0700

    Improved test environment and wrapper script

--- docker-tests.sh
+++ docker-tests.sh
@@ -12,17 +12,17 @@ ARGS="$ARGS taskcluster/tc-worker-env";
 TAGS='qemu network system native';
 
 if [[ "$@" == go\ * ]]; then
-  docker run $ARGS $@;
+  docker run $ARGS "$@";
 elif [[ "$1" == -- ]]; then
   shift;
-  docker run $ARGS $@;
+  docker run $ARGS "$@";
 elif [[ "$@" == bash ]]; then
   docker run $ARGS bash --login;
 elif [[ "$@" == "" ]]; then
   docker run $ARGS go test -race -tags "$TAGS" -p 1 -v \
   `go list ./... | grep -v ^github.com/taskcluster/taskcluster-worker/vendor/`;
 else
-  docker run $ARGS go test -v -race -tags "$TAGS" -p 1 $@;
+  docker run $ARGS go test -v -race -tags "$TAGS" -p 1 "$@";
 fi;
 
 if [[ "$?" != "0" ]]; then
