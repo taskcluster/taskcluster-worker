9523c47a190bcc9f5414ba81a7fef8eb04341209
Jonas Finnemann Jensen <jopsen@gmail.com>
2017-06-14 15:15:37 -0700

    Add LDFLAGS and a few cheap sanity checks

--- commands/version/doc.go
+++ commands/version/doc.go
@@ -2,3 +2,7 @@
 // git revision, these values are also exported through methods so that they
 // can be read from other packages.
 package version
+
+import "github.com/taskcluster/taskcluster-worker/runtime/util"
+
+var debug = util.Debug("version")
