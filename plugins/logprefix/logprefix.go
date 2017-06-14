f9896033698dc66a926ac0e1a51027c580b80604
Jonas Finnemann Jensen <jopsen@gmail.com>
2017-06-14 15:23:56 -0700

    Renamed logprefix entries to start with a lowercase for consistency, why because nobody is stopping me

--- plugins/logprefix/logprefix.go
+++ plugins/logprefix/logprefix.go
@@ -71,10 +71,10 @@ func (p *plugin) NewTaskPlugin(options plugins.TaskPluginOptions) (plugins.TaskP
 	taskCount := atomic.AddInt64(&p.taskCount, 1) - 1
 
 	keys := map[string]string{
-		"TaskId":            options.TaskContext.TaskID,
-		"RunId":             strconv.Itoa(options.TaskContext.RunID),
-		"HostBootTime":      p.bootTime,
-		"TasksSinceStartup": strconv.FormatInt(taskCount, 10),
+		"taskId":            options.TaskContext.TaskID,
+		"runId":             strconv.Itoa(options.TaskContext.RunID),
+		"hostBootTime":      p.bootTime,
+		"tasksSinceStartup": strconv.FormatInt(taskCount, 10),
 	}
 
 	// Construct list of all keys (so we can sort it)
