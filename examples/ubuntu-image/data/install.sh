7941745fd52acaeb3f2d7313f601e113977131db
Jonas Finnemann Jensen <jopsen@gmail.com>
2017-06-16 15:51:00 -0700

    Change ubuntu image to build automatically

--- examples/ubuntu-image/data/install.sh
+++ examples/ubuntu-image/data/install.sh
@@ -1,33 +1,25 @@
-#!/bin/bash -e
+#!/bin/bash
 
-# Find location of the script no matter where it's located
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo ' - Removing password from 'worker' user'
+passwd -d worker
 
-echo '### Installing taskcluster-worker qemu-guest-tools'
-cp "$DIR"/taskcluster-worker /usr/local/bin/
+echo ' - Installing taskcluster-worker qemu-guest-tools'
+cp /tmp/custom-data/taskcluster-worker /usr/local/bin/taskcluster-worker
 chmod +x /usr/local/bin/taskcluster-worker
+cp /tmp/custom-data/taskcluster-worker.service /etc/systemd/system/taskcluster-worker.service
+chmod 644 /etc/systemd/system/taskcluster-worker.service
+systemctl enable taskcluster-worker.service
 
-echo '### Update packages'
-apt-get update -y
-apt-get upgrade -y
-
-echo '### Installing docker'
+echo ' - Installing docker'
 apt-get install -y apt-transport-https ca-certificates
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo 'deb https://apt.dockerproject.org/repo ubuntu-xenial main' > /etc/apt/sources.list.d/docker.list
 apt-get update -y
 apt-get install -y docker-engine
 
-echo '### Installing build dependencies'
+echo ' - Installing build dependencies'
 apt-get install -y git curl screen nano build-essential
 
-echo '### Installing test script'
-cp "$DIR/clone-and-exec.sh" /usr/local/bin/clone-and-exec.sh;
+echo ' - Installing test script'
+cp '/tmp/custom-data/clone-and-exec.sh' /usr/local/bin/clone-and-exec.sh;
 chmod +x /usr/local/bin/clone-and-exec.sh;
-
-echo '### Installing systemd service'
-cp "$DIR/taskcluster-worker.service" /etc/systemd/system/
-chmod 644 /etc/systemd/system/taskcluster-worker.service
-systemctl enable taskcluster-worker.service
-
-echo '### Setup done'
--- examples/ubuntu-image/download.sh
+++ examples/ubuntu-image/download.sh
@@ -11,9 +11,8 @@ fi
 echo '### Checking sha256sum'
 echo "$INSTALLER_SHA256 $INSTALLER_ISO" | sha256sum -c -
 
-if ! [ -f "$DIR/cache/$SETUP_IMAGE_NAME" ]; then
-  echo "### Downloading $SETUP_IMAGE_NAME"
-  curl -L "https://s3-us-west-2.amazonaws.com/$S3_BUCKET/$S3_PREFIX/$SETUP_IMAGE_NAME" > "$DIR/cache/$SETUP_IMAGE_NAME"
-fi
-
 echo "Not downloading $WORKER_IMAGE_NAME as it is easy to rebuild"
+#if ! [ -f "$DIR/cache/$WORKER_IMAGE_NAME" ]; then
+#  echo "### Downloading $WORKER_IMAGE_NAME"
+#  curl -L "https://s3-us-west-2.amazonaws.com/$S3_BUCKET/$S3_PREFIX/$WORKER_IMAGE_NAME" > "$DIR/cache/$WORKER_IMAGE_NAME"
+#fi
