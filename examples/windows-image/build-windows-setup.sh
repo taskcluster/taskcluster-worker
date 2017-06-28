aa774387d38e6b83d4ead8f79f0bd3f6abfc0116
Jonas Finnemann Jensen <jopsen@gmail.com>
2017-06-27 17:37:07 -0700

    Added windows image recipe

---
+++ examples/windows-image/build-windows-setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -e
+
+# Load constants
+source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/constants.sh"
+
+# Create temporary files
+DATA=`mktemp -d`;
+ISO="$DIR/cache/data.iso";
+TCWORKER=`mktemp`;
+
+echo '### Building taskcluster-worker for host (for building image)'
+go build -o "$TCWORKER" github.com/taskcluster/taskcluster-worker
+
+echo '### Building taskcluster-worker for image (for use inside image)'
+CGO_ENABLED=1 GOOS=windows GOARCH=amd64 go build -o "$DATA/taskcluster-worker.exe" \
+  github.com/taskcluster/taskcluster-worker
+
+echo '### Packaging data.iso'
+cp "$DIR/data"/* "$DATA/"
+7z x -o"$DATA/" "$VIRTIO_WIN_ISO"
+genisoimage -vJrV DATA_VOLUME -input-charset utf-8 -o "$ISO" "$DATA"
+
+echo "### Building $WORKER_IMAGE_NAME"
+"$TCWORKER" qemu-build --boot "$INSTALLER_ISO" --cdrom "$ISO" --size "$DISKSIZE" \
+  from-new "$DIR/machine.json" "$DIR/cache/$WORKER_IMAGE_NAME"
+
+echo '### Removing temporary files'
+rm -rf "$DATA"
+rm -f "$ISO" "$TCWORKER"
