3bb5297b8eaea38d71ed6a29a1bf746777654ce0
Jonas Finnemann Jensen <jopsen@gmail.com>
2017-10-05 15:00:48 -0700

    More error annotation to make bug 1405695 easier to find

--- runtime/artifact.go
+++ runtime/artifact.go
@@ -2,13 +2,15 @@ package runtime
 
 import (
 	"encoding/json"
-	"errors"
+	"io"
+	"io/ioutil"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"strconv"
 	"time"
 
+	"github.com/pkg/errors"
 	got "github.com/taskcluster/go-got"
 	tcclient "github.com/taskcluster/taskcluster-client-go"
 	"github.com/taskcluster/taskcluster-client-go/queue"
@@ -49,7 +51,7 @@ func (context *TaskContext) UploadS3Artifact(artifact S3Artifact) error {
 		StorageType: "s3",
 	})
 	if err != nil {
-		return err
+		panic(errors.Wrap(err, "failed to Marshal json that should have worked"))
 	}
 
 	parsed, err := context.createArtifact(artifact.Name, req)
@@ -57,9 +59,8 @@ func (context *TaskContext) UploadS3Artifact(artifact S3Artifact) error {
 		return err
 	}
 	var resp queue.S3ArtifactResponse
-	err = json.Unmarshal(parsed, &resp)
-	if err != nil {
-		return err
+	if err = json.Unmarshal(parsed, &resp); err != nil {
+		panic(errors.Wrap(err, "failed to parse JSON that have been parsed before"))
 	}
 
 	return putArtifact(resp.PutURL, artifact.Mimetype, artifact.Stream, artifact.AdditionalHeaders)
@@ -126,11 +127,11 @@ func (context *TaskContext) createArtifact(name string, req []byte) ([]byte, err
 func putArtifact(urlStr, mime string, stream ioext.ReadSeekCloser, additionalArtifacts map[string]string) error {
 	u, err := url.Parse(urlStr)
 	if err != nil {
-		return err
+		panic(errors.Wrap(err, "failed to parse URL"))
 	}
-	contentLength, err := stream.Seek(0, 2)
+	contentLength, err := stream.Seek(0, io.SeekEnd)
 	if err != nil {
-		return err
+		return errors.Wrap(err, "failed to seek end of stream for content-length detection")
 	}
 
 	header := make(http.Header)
@@ -147,9 +148,9 @@ func putArtifact(urlStr, mime string, stream ioext.ReadSeekCloser, additionalArt
 	}
 	for {
 		attempts++
-		_, err := stream.Seek(0, 0)
+		_, err := stream.Seek(0, io.SeekStart)
 		if err != nil {
-			return err
+			return errors.Wrap(err, "Failed to seek start before uploading stream")
 		}
 		req := &http.Request{
 			Method:        "PUT",
@@ -158,20 +159,31 @@ func putArtifact(urlStr, mime string, stream ioext.ReadSeekCloser, additionalArt
 			ProtoMajor:    1,
 			ProtoMinor:    1,
 			Header:        header,
-			Body:          stream,
 			ContentLength: contentLength,
+			Body:          stream,
+			GetBody: func() (io.ReadCloser, error) {
+				// In case we have to follow any redirects, which shouldn't happen
+				if _, serr := stream.Seek(0, io.SeekStart); serr != nil {
+					return nil, errors.Wrap(serr, "failed to seek to start of stream")
+				}
+				return ioutil.NopCloser(stream), nil
+			},
 		}
 		resp, err := client.Do(req)
 		if err != nil {
-			return err
+			if attempts < 10 {
+				time.Sleep(backoff.Delay(attempts))
+				continue
+			}
+			return errors.Wrap(err, "failed send request")
 		}
 		defer resp.Body.Close()
 		if resp.StatusCode/100 == 4 {
 			httpErr, err := httputil.DumpResponse(resp, true)
 			if err != nil {
-				return err
+				return errors.Errorf("HTTP status: %d, and error dumping response: %s", resp.StatusCode, err)
 			}
-			return errors.New(string(httpErr))
+			return errors.Errorf("HTTP status: %d, response: %s", resp.StatusCode, string(httpErr))
 		}
 		if resp.StatusCode/100 == 5 {
 			// TODO: Make this configurable
@@ -181,9 +193,9 @@ func putArtifact(urlStr, mime string, stream ioext.ReadSeekCloser, additionalArt
 			} else {
 				httpErr, err := httputil.DumpResponse(resp, true)
 				if err != nil {
-					return err
+					return errors.Errorf("HTTP status: %d, and error dumping response: %s", resp.StatusCode, err)
 				}
-				return errors.New(string(httpErr))
+				return errors.Errorf("HTTP status: %d, response: %s", resp.StatusCode, string(httpErr))
 			}
 		}
 		// If we've made it here, the upload has succeeded
