5eee180b702e3cf7e5685621347774abc2de77b0
Jonas Finnemann Jensen <jopsen@gmail.com>
2017-12-10 23:19:52 -0800

    Add provisionerId and workerType to runtime.Environment
    These are useful when a plugin wants to know what workerType
    it is operating on... Notably, the cache plugin needs this
    in-order to be able to lookup purge-cache requests from the
    taskcluster-purge-cache service. But it's arguable that
    order plugins will find similar uses for this information.

--- engines/mock/mockengine.go
+++ engines/mock/mockengine.go
@@ -34,6 +34,18 @@ func (e engineProvider) NewEngine(options engines.EngineOptions) (engines.Engine
 	if options.Environment.Monitor == nil {
 		panic("EngineOptions.Environment.Monitor is nil, this is a contract violation")
 	}
+	if options.Environment.ProvisionerID == "" {
+		panic("EngineOptions.Environment.ProvisionerID is empty string, this is a contract violation")
+	}
+	if options.Environment.WorkerType == "" {
+		panic("EngineOptions.Environment.WorkerType is empty string, this is a contract violation")
+	}
+	if options.Environment.WorkerGroup == "" {
+		panic("EngineOptions.Environment.WorkerGroup is empty string, this is a contract violation")
+	}
+	if options.Environment.WorkerID == "" {
+		panic("EngineOptions.Environment.WorkerID is empty string, this is a contract violation")
+	}
 	if options.Monitor == nil {
 		panic("EngineOptions.Monitor is nil, this is a contract violation")
 	}
