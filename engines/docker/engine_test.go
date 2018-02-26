952f1fa5d7afa428639a38484e053d35320dc2af
Chinmay Jayaram Kousik <chinmaykousik1@gmail.com>
2018-02-26 20:16:36 +0530

    Added kill, env, and artifact tests for docker engine

--- engines/docker/engine_test.go
+++ engines/docker/engine_test.go
@@ -41,8 +41,72 @@ func TestLogging(t *testing.T) {
 		}`,
 	}
 
-	c.TestLogTarget()
-	c.TestLogTargetWhenFailing()
-	c.TestSilentTask()
+	c.Test()
+}
+
+func TestKill(t *testing.T) {
+	c := enginetest.KillTestCase{
+		EngineProvider: provider,
+		Target:         `hello-world`,
+		Payload: `{
+			"command": ["sh", "-c", "echo 'hello-world' && sleep 30 && true"],
+			"image": {
+				"repository": "ubuntu",
+				"tag": "latest"
+			}
+		}`,
+	}
+
+	c.Test()
+}
+
+func TestEnvironmentVariables(t *testing.T) {
+	c := enginetest.EnvVarTestCase{
+		EngineProvider: provider,
+		VariableName:   "TEST_ENV_VAR",
+		InvalidVariableNames: []string{
+			"#=#",
+		},
+		Payload: `{
+			"command": ["sh", "-c", "echo $TEST_ENV_VAR && true"],
+			"image": {
+				"repository": "ubuntu",
+				"tag": "latest"
+			}
+		}`,
+	}
+
+	c.TestPrintVariable()
+	c.TestVariableNameConflict()
+	c.TestInvalidVariableNames()
+	c.Test()
+}
+
+func TestArtifacts(t *testing.T) {
+	c := enginetest.ArtifactTestCase{
+		EngineProvider:     provider,
+		Text:               "[hello-world]",
+		TextFilePath:       "folder/hello.txt",
+		FileNotFoundPath:   "no-such-file.txt",
+		FolderNotFoundPath: "no-such-folder/",
+		NestedFolderFiles: []string{
+			"hello.txt",
+			"sub-folder/hello2.txt",
+		},
+		NestedFolderPath: "folder/",
+		Payload: `{
+			"command": ["sh", "-ec", "mkdir -p folder/sub-folder; echo '[hello-world]' > folder/hello.txt; echo '[hello-world]' > folder/sub-folder/hello2.txt"],
+			"image": {
+				"repository": "ubuntu",
+				"tag": "latest"
+			}
+		}`,
+	}
+
+	// c.TestExtractTextFile()
+	// c.TestExtractFileNotFound()
+	// c.TestExtractFolderNotFound()
+	// c.TestExtractNestedFolderPath()
+	// c.TestExtractFolderHandlerInterrupt()
 	c.Test()
 }
