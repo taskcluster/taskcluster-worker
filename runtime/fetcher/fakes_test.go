0afe512a5d57c6bcbd34ac50bb95fd8e6621d346
Jonas Finnemann Jensen <jopsen@gmail.com>
2018-04-21 15:13:55 +0200

    Added a fakeReference for better testing

--- runtime/fetcher/fakes_test.go
+++ runtime/fetcher/fakes_test.go
@@ -3,6 +3,8 @@ package fetcher
 import (
 	"bytes"
 	"context"
+	"crypto/rand"
+	"errors"
 	"io"
 	"sync"
 	"testing"
@@ -75,4 +77,152 @@ func TestFakeWriteSeekReseter(t *testing.T) {
 	_, err = io.Copy(w, bytes.NewBufferString(" test again"))
 	require.NoError(t, err)
 	require.Equal(t, w.String(), "test again test again")
+}
+
+type fakeReference struct {
+	ScopesValue  [][]string
+	HashKeyValue string
+	Data         []byte
+	Reset        bool
+	Err          error
+}
+
+func (r *fakeReference) HashKey() string {
+	return r.HashKeyValue
+}
+
+func (r *fakeReference) Scopes() [][]string {
+	return r.ScopesValue
+}
+
+func (r *fakeReference) Fetch(context Context, target WriteReseter) error {
+	i := 0
+	// Write a few small blocks
+	if i+1 < len(r.Data) {
+		n, err := target.Write(r.Data[i : i+1])
+		if err != nil {
+			return err
+		}
+		if n != 1 {
+			panic(errors.New("Expected n = 1 when no error was returned"))
+		}
+		i += n
+	}
+	if i+7 < len(r.Data) {
+		n, err := target.Write(r.Data[i : i+7])
+		if err != nil {
+			return err
+		}
+		if n != 7 {
+			panic(errors.New("Expected n = 7 when no error was returned"))
+		}
+		i += n
+	}
+	if i+3 < len(r.Data) {
+		n, err := target.Write(r.Data[i : i+3])
+		if err != nil {
+			return err
+		}
+		if n != 3 {
+			panic(errors.New("Expected n = 3 when no error was returned"))
+		}
+		i += n
+	}
+	if i < len(r.Data) {
+		n, err := target.Write(r.Data[i:])
+		if err != nil {
+			return err
+		}
+		if n != len(r.Data)-i {
+			panic(errors.New("Expected n = len(r.Data)-i when no error was returned"))
+		}
+	}
+	if r.Reset {
+		r.Reset = false
+		if err := target.Reset(); err != nil {
+			return nil
+		}
+		return r.Fetch(context, target)
+	}
+	return r.Err
+}
+
+func TestFakeReference(t *testing.T) {
+	ctx := &fakeContext{Context: context.Background()}
+	// Create a random blob
+	blob := make([]byte, 16*1024+27)
+	_, rerr := rand.Read(blob)
+	require.NoError(t, rerr, "failed to created random data for testing")
+
+	t.Run("Empty String without Reset", func(t *testing.T) {
+		r := &fakeReference{
+			Data:  []byte(""),
+			Reset: false,
+			Err:   nil,
+		}
+		w := &fakeWriteReseter{}
+		err := r.Fetch(ctx, w)
+		require.NoError(t, err)
+		require.Equal(t, "", w.String())
+	})
+
+	t.Run("hello-world without Reset", func(t *testing.T) {
+		r := &fakeReference{
+			Data:  []byte("hello-world"),
+			Reset: false,
+			Err:   nil,
+		}
+		w := &fakeWriteReseter{}
+		err := r.Fetch(ctx, w)
+		require.NoError(t, err)
+		require.Equal(t, "hello-world", w.String())
+	})
+
+	t.Run("blob without Reset", func(t *testing.T) {
+		r := &fakeReference{
+			Data:  blob,
+			Reset: false,
+			Err:   nil,
+		}
+		w := &fakeWriteReseter{}
+		err := r.Fetch(ctx, w)
+		require.NoError(t, err)
+		require.True(t, bytes.Compare(blob, w.buffer) == 0, "expected blob == w.buffer")
+	})
+
+	t.Run("Empty String with Reset", func(t *testing.T) {
+		r := &fakeReference{
+			Data:  []byte(""),
+			Reset: true,
+			Err:   nil,
+		}
+		w := &fakeWriteReseter{}
+		err := r.Fetch(ctx, w)
+		require.NoError(t, err)
+		require.Equal(t, "", w.String())
+	})
+
+	t.Run("hello-world with Reset", func(t *testing.T) {
+		r := &fakeReference{
+			Data:  []byte("hello-world"),
+			Reset: true,
+			Err:   nil,
+		}
+		w := &fakeWriteReseter{}
+		err := r.Fetch(ctx, w)
+		require.NoError(t, err)
+		require.Equal(t, "hello-world", w.String())
+	})
+
+	t.Run("blob with Reset", func(t *testing.T) {
+		r := &fakeReference{
+			Data:  blob,
+			Reset: true,
+			Err:   nil,
+		}
+		w := &fakeWriteReseter{}
+		err := r.Fetch(ctx, w)
+		require.NoError(t, err)
+		require.True(t, bytes.Compare(blob, w.buffer) == 0, "expected blob == w.buffer")
+	})
 }
