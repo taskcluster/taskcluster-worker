891b7e08289783d90105eeea013e34ca3d5aaaf0
Jonas Finnemann Jensen <jopsen@gmail.com>
2018-04-24 13:11:53 +0200

    Test for mount-point validation of AttachVolume

---
+++ engines/docker/sandboxbuilder_test.go
@@ -0,0 +1,45 @@
+package dockerengine
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestValidateMountPoint(t *testing.T) {
+	invalidMountPoints := []string{
+		"",
+		"/",
+		"//",
+		"/test",
+		"test",
+		"test/",
+		"/test/a",
+		"/test/./",
+		"../",
+		"/..",
+		"/test/../",
+		string([]byte{'/', 't', 0, 't', '/'}), // try with \0
+		"/test\\something/",
+	}
+	validMountPoints := []string{
+		"/test/",
+		"/test/test/",
+		"/tmp/",
+		"/var/lib/docker/",
+		"/mnt/my-folder/",
+	}
+
+	for _, mountPoint := range invalidMountPoints {
+		t.Run(`"`+mountPoint+`"`, func(t *testing.T) {
+			err := validateMountPoint(mountPoint)
+			assert.Error(t, err, "expected '"+mountPoint+"' to be valid")
+		})
+	}
+	for _, mountPoint := range validMountPoints {
+		t.Run(`"`+mountPoint+`"`, func(t *testing.T) {
+			err := validateMountPoint(mountPoint)
+			assert.NoError(t, err, "expected '%s' to be valid", mountPoint)
+		})
+	}
+}
