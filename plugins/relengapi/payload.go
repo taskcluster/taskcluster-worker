fc8af6b9f314e2ec3366a5aa9ec932a20334fb94
Wander Lairson Costa <wcosta@mozilla.com>
2018-05-10 17:44:25 +0000

    Implement relengapi proxy

---
+++ plugins/relengapi/payload.go
@@ -0,0 +1,27 @@
+package relengapi
+
+import (
+	schematypes "github.com/taskcluster/go-schematypes"
+	"github.com/taskcluster/taskcluster-worker/runtime/util"
+)
+
+var payloadSchema = schematypes.Object{
+	Properties: schematypes.Properties{
+		"enableRelengAPIProxy": schematypes.Boolean{
+			Title: "Enable Proxy",
+			Description: util.Markdown(`
+        The relengapi proxy forwards requests to the proxy 'relengapi'. The proxy
+        is disabled by default, this option can be used to enable it per-task.
+
+        Please refer to engine specific documentation for how to access the
+        proxy, often it is something like: 'http://<hostname>/<proxy>/<...>',
+        hence, forwarding to the queue would be
+        'http://<hostname>/relengapi/<request>/...'.
+      `),
+		},
+	},
+}
+
+type payload struct {
+	EnableRelengAPIProxy bool `json:"enableRelengAPIProxy"`
+}
---
+++ plugins/relengapi/relengapi.go
@@ -0,0 +1,170 @@
+package relengapi
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+	"time"
+
+	"github.com/pkg/errors"
+	schematypes "github.com/taskcluster/go-schematypes"
+	"github.com/taskcluster/taskcluster-worker/engines"
+	"github.com/taskcluster/taskcluster-worker/plugins"
+	"github.com/taskcluster/taskcluster-worker/runtime"
+)
+
+const tmpTokenLifetime time.Duration = 10 * time.Minute
+const tmpTokenSkew time.Duration = 10 * time.Second
+
+type provider struct {
+	plugins.PluginProviderBase
+}
+
+type plugin struct {
+	plugins.PluginBase
+	config config
+}
+
+type taskPlugin struct {
+	plugins.TaskPluginBase
+	monitor           runtime.Monitor
+	proxy             httputil.ReverseProxy
+	context           *runtime.TaskContext
+	tmpToken          string
+	tmpTokenGoodUntil time.Time
+	permissions       []string
+	relengapiHost     string
+}
+
+func init() {
+	plugins.Register("relengapi", provider{})
+}
+
+func (provider) ConfigSchema() schematypes.Schema {
+	return configSchema
+}
+
+func (p *plugin) PayloadSchema() schematypes.Object {
+	return payloadSchema
+}
+
+func (provider) NewPlugin(options plugins.PluginOptions) (plugins.Plugin, error) {
+	var c config
+	schematypes.MustValidateAndMap(configSchema, options.Config, &c)
+	if c.Host == "" {
+		c.Host = "api.pub.build.mozilla.org"
+	}
+
+	if c.Token == "" {
+		err := errors.New("Token value not configured for relengapi plugin")
+		debug(fmt.Sprintln(err))
+		options.Monitor.Error(err)
+		return nil, err
+	}
+
+	return &plugin{
+		PluginBase: plugins.PluginBase{},
+		config:     c,
+	}, nil
+}
+
+func (p *plugin) NewTaskPlugin(options plugins.TaskPluginOptions) (plugins.TaskPlugin, error) {
+	var P payload
+	schematypes.MustValidateAndMap(payloadSchema, options.Payload, &P)
+
+	// If disabled, we return nothing
+	if !P.EnableRelengAPIProxy {
+		return plugins.TaskPluginBase{}, nil
+	}
+
+	debug(fmt.Sprintf("Starting relengapi plugin for task %s", options.TaskContext.TaskID))
+
+	allPerms := []string{
+		"base.tokens.tmp.issue",
+		"base.tokens.usr.issue",
+		"base.tokens.usr.revoke.my",
+		"base.tokens.usr.view.my",
+		"clobberer.post.clobber",
+		"tooltool.download.internal",
+		"tooltool.download.public",
+		"tooltool.upload.internal",
+		"tooltool.upload.public",
+	}
+
+	perms := []string{}
+	for _, p := range allPerms {
+		if options.TaskContext.HasScopes([]string{"worker:relengapi-proxy:" + p}) {
+			perms = append(perms, p)
+		}
+	}
+
+	tp := &taskPlugin{
+		monitor:       options.Monitor,
+		context:       options.TaskContext,
+		permissions:   perms,
+		relengapiHost: p.config.Host,
+	}
+
+	// stolen from relengapi-proxy
+	director := func(req *http.Request) {
+		req.URL.Scheme = "https"
+		req.URL.Host = p.config.Host
+		req.Host = p.config.Host
+		tok, err := tp.getToken(p.config.Token)
+		if err != nil {
+			err = errors.Wrap(err, "Error retrieving token")
+			debug(fmt.Sprintln(err))
+			options.TaskContext.Log(err)
+		} else {
+			req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", tok))
+		}
+		options.TaskContext.Log(fmt.Sprint(req.Method, req.URL))
+	}
+
+	tp.proxy = httputil.ReverseProxy{
+		Director: director,
+	}
+
+	return tp, nil
+}
+
+func (p *taskPlugin) BuildSandbox(sandboxBuilder engines.SandboxBuilder) error {
+	err := sandboxBuilder.AttachProxy("relengapi", p)
+	if err == engines.ErrFeatureNotSupported {
+		// Fire off a warning, and then do nothing...
+		p.monitor.ReportWarning(err, "plugin 'relengapi' is enabled, but the engine doesn't support proxy attachments")
+		return nil
+	}
+	if err == engines.ErrNamingConflict {
+		err = runtime.NewMalformedPayloadError("the proxy name 'relengapi' is already in use")
+		debug(err.Error())
+		return err
+	}
+	if _, ok := runtime.IsMalformedPayloadError(err); ok {
+		// the name "relengapi" is not allowed by the engine, we assume it to be safe,
+		// so if it's not we'll panic
+		panic(errors.Wrap(err, "proxy name 'relengapi' is not permitted by the engine"))
+	}
+	return nil
+}
+
+func (p *taskPlugin) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	p.proxy.ServeHTTP(w, r)
+}
+
+func (p *taskPlugin) getToken(issuingToken string) (string, error) {
+	now := time.Now()
+	if now.After(p.tmpTokenGoodUntil) {
+		expires := now.Add(tmpTokenLifetime)
+		debug("Generating new temporary token; expires at " + expires.String())
+		urlPrefix := fmt.Sprintf("https://%s", p.relengapiHost)
+		tok, err := getTmpToken(
+			urlPrefix, issuingToken, expires, p.permissions)
+		if err != nil {
+			return "", err
+		}
+		p.tmpToken = tok
+		p.tmpTokenGoodUntil = expires.Add(-tmpTokenSkew)
+	}
+	return p.tmpToken, nil
+}
