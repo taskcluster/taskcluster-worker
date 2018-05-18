66add985fca8b4d0f97b612b23d15e2e621dae1e
Jonas Finnemann Jensen <jopsen@gmail.com>
2018-05-18 15:19:17 +0200

    Fixing linting, typo in relengapi plugin

--- plugins/relengapi/doc.go
+++ plugins/relengapi/doc.go
@@ -1,5 +1,5 @@
 // Package relengapi provides a taskcluster-worker plugin that exposes a proxy
-// that foward requests to relengapi.
+// that forward requests to relengapi.
 package relengapi
 
 import "github.com/taskcluster/taskcluster-worker/runtime/util"
