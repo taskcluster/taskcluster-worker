4593bd1d551b0239c845ec5a92f528e92f393e86
Jonas Finnemann Jensen <jopsen@gmail.com>
2018-05-25 16:14:52 +0200

    Fixed documentation comments

--- plugins/relengapi/payload.go
+++ plugins/relengapi/payload.go
@@ -10,13 +10,13 @@ var payloadSchema = schematypes.Object{
 		"enableRelengAPIProxy": schematypes.Boolean{
 			Title: "Enable Proxy",
 			Description: util.Markdown(`
-        The relengapi proxy forwards requests to the proxy 'relengapi'. The proxy
-        is disabled by default, this option can be used to enable it per-task.
+				The relengapi proxy forwards requests to the proxy 'relengapi'. The proxy
+				is disabled by default, this option can be used to enable it per-task.
 
-        Please refer to engine specific documentation for how to access the
-        proxy, often it is something like: 'http://<hostname>/<proxy>/<...>',
-        hence, forwarding to the queue would be
-        'http://<hostname>/relengapi/<request>/...'.
+				Please refer to engine specific documentation for how to access the
+				proxy, often it is something like: 'http://<hostname>/<proxy>/<...>',
+				hence, forwarding to the queue would be
+				'http://<hostname>/relengapi/<request>/...'.
       `),
 		},
 	},
--- plugins/tcproxy/payload.go
+++ plugins/tcproxy/payload.go
@@ -10,15 +10,15 @@ var payloadSchema = schematypes.Object{
 		"disableTaskclusterProxy": schematypes.Boolean{
 			Title: "Disable Proxy",
 			Description: util.Markdown(`
-        The taskcluster proxy forwards requests to the proxy 'tcproxy'
-        while attaching a request signature covering 'task.scopes'. The proxy
-        is enabled by default, this option can be used to disable it per-task.
+				The taskcluster proxy forwards requests to the proxy 'tcproxy'
+				while attaching a request signature covering 'task.scopes'. The proxy
+				is enabled by default, this option can be used to disable it per-task.
 
-        Please refer to engine specific documentation for how to access the
-        proxy, often it is something like: 'http://<hostname>/<proxy>/<...>',
-        hence, forwarding to the queue would be
-        'http://<hostname>/tcproxy/queue.taskcluster.net/...'.
-      `),
+				Please refer to engine specific documentation for how to access the
+				proxy, often it is something like: 'http://<hostname>/<proxy>/<...>',
+				hence, forwarding to the queue would be
+				'http://<hostname>/tcproxy/queue.taskcluster.net/...'.
+			`),
 		},
 	},
 }
